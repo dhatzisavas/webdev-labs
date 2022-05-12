5f24d42da00b60a2a226de44f67d408c83bb9037
dhatzisavas <dhatzisavas@live.com>
2022-05-12 13:06:16 -0500

    lab updates

--- lab06/02-theme-switcher/index.js
+++ lab06/02-theme-switcher/index.js
@@ -11,7 +11,7 @@
 const changeTheme = (theme) =>
    document.querySelector("body").className = theme
 
-document.querySelector("#desert").addEventListener('click', () => {changeTheme("desert")})
+document.querySelector("#desert").addEventListener('click', () => changeTheme("desert"))
 document.querySelector("#ocean").addEventListener('click', () => {changeTheme("ocean")})
 document.querySelector("#high-contrast").addEventListener('click', () => {changeTheme("high-contrast")})
 document.querySelector("#default").addEventListener('click', () => {changeTheme("")})
