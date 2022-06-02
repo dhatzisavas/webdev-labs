14c4681972c2a26a6576ac0e8323debc5fa9141b
dhatzisavas <dhatzisavas@live.com>
2022-06-01 21:38:47 -0500

    profile js done

---
+++ lab10/src/Profile.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { getHeaders } from './utils';
+
+class Profile extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // constructor logic
+        this.state = { profile : [] }
+        this.fetchProfile = this.fetchProfile.bind(this)
+        // this.componentDidMount = this.componentDidMount.bind(this)
+        // this.componentDidMount()
+        console.log('Profile component created');
+    }
+
+    fetchProfile() {
+        fetch('/api/profile', {
+            method: "GET",
+            headers: getHeaders()
+        })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ profile: data })
+            })
+    }
+
+    componentDidMount() {
+        this.fetchProfile()
+        console.log('Profile component mounted');
+    }
+
+    render() {
+        return (
+            <div className="currentuser">
+                <img src={this.state.profile.image_url} alt="user profile pic" />
+                <div className="currentusername">{this.state.profile.username}</div>
+            </div>
+        );
+    }
+}
+
+export default Profile;
